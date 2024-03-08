2cdd01f015a6882cb5f2f7ae3efea0d73c6ccb5d
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2024-03-08 20:14:42 +0700

    Initial commit

---
+++ src/main/java/com/project/mangerhotel/MangerHotelApplication.java
@@ -0,0 +1,13 @@
+package com.project.mangerhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MangerHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MangerHotelApplication.class, args);
+    }
+
+}
