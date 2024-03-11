428e7d8504c28252e00054fc4b9af3c100f18213
Long Ngo <126145913+NVCLong@users.noreply.github.com>
2024-03-11 10:56:03 +0700

    Initial commit

---
+++ frontend/myapp/src/App.js
@@ -0,0 +1,23 @@
+import logo from './logo.svg';
+import './App.css';
+
+import {
+    BrowserRouter,
+    Routes,
+    Route
+} from "react-router-dom";
+import Login from "./page/Login";
+import Register from "./page/Register";
+function App() {
+
+  return (
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login/>} />
+            <Route path="/register" element={<Register/>} />
+        </Routes>
+      </BrowserRouter>
+  );
+}
+
+export default App;
