513f71c4dba31dcc21787a2856ba7ffc50ed2aca
LongNgo <ngovucaolong9118.thd@gmail.com>
2024-05-14 09:25:21 +0700

    update

--- backend/src/main/java/com/project/mangerhotel/config/CorsConfig.java
+++ backend/src/main/java/com/project/mangerhotel/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","https://manger-hotel.vercel.app")
                 .allowedMethods("GET","POST","PUT","DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
